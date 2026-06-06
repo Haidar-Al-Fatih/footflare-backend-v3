3c14413a23e6b248264a456f307370c79ec348e1
Haidar-Al-Fatih <haidaralfatih2004@gmail.com>
2026-06-07 04:29:19 +0700

    Web Admin Finished, minus brand sama cate masih id

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,14 +9,27 @@ class Product extends Model
 {
     use HasFactory;
 
+    protected $table = 'products';
+
     protected $fillable = [
-        'id',
-        'brand_id',
-        'category_id',
-        'name',
-        'description',
-        'price',
-        'discount_percentage',
-        'thumbnail_url'
+        'name', 
+        'brand_id', 
+        'category_id', 
+        'price', 
+        'discount_percentage', 
+        'thumbnail_url', 
+        'description'
     ];
+
+    // Relasi ke Model Brand (Menggunakan full namespace untuk mencegah Class not found)
+    public function brand()
+    {
+        return $this->belongsTo(\App\Models\Brand::class, 'brand_id');
+    }
+
+    // Relasi ke Model Category
+    public function category()
+    {
+        return $this->belongsTo(\App\Models\Category::class, 'category_id');
+    }
 }
